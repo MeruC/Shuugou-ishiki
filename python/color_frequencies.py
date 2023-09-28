f49ac3cfd400da2b27a7b288fc7c8ef9f55f1c30
Yatagarasu <36064011+MeruC@users.noreply.github.com>
2023-09-28 11:08:09 +0800

    Update color_frequencies.py

--- python/color_frequencies.py
+++ python/color_frequencies.py
@@ -7,6 +7,22 @@ def color_frequencies(*colors):
             frequencies[color] = 1
     return frequencies
 
-colors = ("red", "green", "blue", "red", "blue")
+#INPUT VERSION
+colors = []
+while True:
+    try:
+        color = input("Enter a color (or 'stop' to finish): ").strip().lower()
+        if color == "stop":
+            break
+        elif color.isalpha():
+            colors.append(color)
+        else:
+            raise ValueError("Invalid input. Please enter a valid color name.")
+    except ValueError as e:
+        print(e)
+
+#PRE DEFINED VERSION
+#colors = ("red", "green", "blue", "red", "blue")
+
 result = color_frequencies(*colors)
 print(result)
